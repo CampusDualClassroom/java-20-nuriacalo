5c2848d7ca2b9b2f425835215dae25e1a585ac59
Nuria Calo <nuriacalomosquera@gmail.com>
2025-05-26 12:51:52 +0200

    exercice 20 change

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -6,11 +6,18 @@ import java.util.List;
 public class Exercise20 {
 
     public static List<Person> getPeopleList() {
-
+        List<Person> peopleList = new ArrayList<>();
+        peopleList.add(new Person("John", "Smith"));
+        peopleList.add(new Teacher("María", "Montessori", "Educación"));
+        peopleList.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
+        peopleList.add(new Doctor("Gregory", "House", "Nefrología e infectología"));
+        return peopleList;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
-
+        for (Person person : stringList) {
+            person.getDetails();
+        }
     }
 
     public static void main(String[] args) {
